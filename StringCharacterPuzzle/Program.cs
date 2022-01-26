cb6bbc98a19e17570fa2a11dfdb4e2c5c913e6ef
Charles Ford <charles.ford1978@gmail.com>
2022-01-26 12:10:42 -0500

    Added output line to display number of characters in the string.

--- StringCharacterPuzzle/Program.cs
+++ StringCharacterPuzzle/Program.cs
@@ -1,3 +1,6 @@
 ﻿string stuff = Console.ReadLine();
 
+Console.WriteLine("");
 Console.WriteLine(stuff);
+Console.WriteLine("");
+Console.WriteLine($"Your string is {stuff.Length} characters long.");
