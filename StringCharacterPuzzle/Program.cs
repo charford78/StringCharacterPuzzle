c1ccf74d7d691ce15dd855f2f45f0ff094b3305e
Charles Ford <charles.ford1978@gmail.com>
2022-01-26 14:05:23 -0500

    Added line to convert string to a character array.  Added code in my if statement to print the middle character in a string with an odd number of characters and the middle two characters in a string with an even number of characters.

--- StringCharacterPuzzle/Program.cs
+++ StringCharacterPuzzle/Program.cs
@@ -1,6 +1,19 @@
 ﻿string stuff = Console.ReadLine();
 
+char[] chars = stuff.ToCharArray();
+
 Console.WriteLine("");
 Console.WriteLine(stuff);
 Console.WriteLine("");
 Console.WriteLine($"Your string is {stuff.Length} characters long.");
+Console.WriteLine("");
+
+if(stuff.Length % 2 == 0)
+{
+    Console.WriteLine("Your string has an even number of characters.");
+    Console.WriteLine($"The middle 2 characters are '{chars[stuff.Length / 2 -1]}', '{chars[stuff.Length / 2]}'.");
+} else
+{
+    Console.WriteLine("Your string has an odd number of characters.");
+    Console.WriteLine($"The middle character is '{chars[stuff.Length / 2]}'.");
+}
